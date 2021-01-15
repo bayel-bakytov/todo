6a3deb47d81f7bd3f44be3e2228e2feb4446dcf7
Bayel <bayel.bakytovich@gmail.com>
2021-01-15 21:12:02 +0600

    Create static media files

--- todo/urls.py
+++ todo/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main.views import homepage,test,go,fizz_buzz
+from main.views import homepage, test
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",homepage,name="home"),
-    path("test/",test,name="test"),
-    path("go/",go,name="go"),
-    path("fizz_buzz/",fizz_buzz,name="fizz_buzz")
-]
+    path("", homepage, name="home"),
+    path("test/", test, name="test"),
+]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
