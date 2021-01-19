7929236191131d3c7071ad53be2a61d8d209fd0a
Bayel <bayel.bakytovich@gmail.com>
2021-01-19 15:40:59 +0600

    added todo closing

--- main/views.py
+++ main/views.py
@@ -43,4 +43,10 @@ def unmark_todo(request,id):
      todo.save() 
      return redirect(test)
 
+def close_todo(request,id):
+    todo = ToDo.objects.get(id=id)
+    todo.is_closed = not todo.is_closed
+    todo.save()
+    return redirect(test)
+
 
